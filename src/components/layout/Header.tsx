d81a2ccf7777a48bca6e2278fbd179c15fe212e3
Lolu <82840714+Lolumajek@users.noreply.github.com>
2026-08-16 00:45:33 -0500

    Make pre-launch landing page links functional

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,7 @@
 import { getNavigation } from "@/lib/content/navigation";
-import { getMarkets } from "@/lib/content/markets";
 import { HeaderClient } from "@/components/layout/HeaderClient";
 
 export function Header() {
   const navigation = getNavigation();
-  const markets = getMarkets();
-
-  return <HeaderClient primary={navigation.primary} markets={markets} />;
+  return <HeaderClient primary={navigation.primary} />;
 }
